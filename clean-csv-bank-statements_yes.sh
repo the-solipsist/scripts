53effa8e45393317d145446fe777cb0e0a4534d6
the-solipsist <the.solipsist@gmail.com>
2018-02-05 23:23:03 +0530

    Create clean-csv-bank-statements_yes.sh

---
+++ clean-csv-bank-statements_yes.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sed -i.bak '/^Txn/,$!d' $1
+sed -i "s/'//g" $1
+sed -i 's/^[ \t]*//' $1
+sed -i '/^$/d' $1
+sed -i '/^Specified/ d' $1
+sed -i '/^Account/ d' $1
+sed -i '/^Opening/ d' $1
+sed -i '/^Closing/ d' $1
+sed -i '1n;/^Txn/d' $1
