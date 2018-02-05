3807bf3b7ed9965ec7ec7c2a8b4d51a39ceeceaf
the-solipsist <the.solipsist@gmail.com>
2018-02-05 23:08:05 +0530

    Create hledger_get-currency-prices.sh

---
+++ hledger_get-currency-prices.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+CUR=("USD" "EUR" "BRL" "AED" "AZN" "BRL" "CHF" "CLP" "EGP" "EUR" "GTQ" "HKD" "IDR" "KES" "LKR" "LTL" "MAD" "MYR" "PHP" "QAR" "SGD" "THB" "TRY" "VND" "ZAR")
+for i in {0..2}
+do
+  curl -s "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${CUR[i]}&to_currency=INR&apikey=[apikey]" | \
+  jq -r '."Realtime Currency Exchange Rate"."5. Exchange Rate"' | \
+  awk -v c=${CUR[i]} '{print "P",strftime("%Y-%m-%d"),c,"₹"$1}' | \
+  tee -a rates.journal
+done
