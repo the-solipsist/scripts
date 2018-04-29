92ad38f87d594381000ce60c60f8c395119f6a05
Vishesh Handa <me@vhanda.in>
2018-04-29 23:15:19 +0200

    Only save the mutualfunds which we have as commodities

--- hledger_get-indian-mf-prices.sh
+++ hledger_get-indian-mf-prices.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+
+hledger stats | grep -o '\bINF\w*' > /tmp/hledger-commodities
+
 curl -s https://www.amfiindia.com/spages/NAVAll.txt | \
-grep -E "INF677K01023|[ISIN]|[symbol]" | \
+grep -f /tmp/hledger-commodities | \
 tr -d '\r' | \
 awk -F";" '{printf("\"%s\" %s ",$2,"₹"$5);system("date -d "$8" +%Y-%m-%d");}' | \
 awk '{print "P",$3,$1,$2 }' | \
