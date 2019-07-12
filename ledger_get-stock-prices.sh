325445e9bbc9d13028575755c21bbbf3cb796ec6
the-solipsist <the.solipsist@gmail.com>
2019-07-12 21:40:20 +0530

    Updated to be able to use both ledger or hledger

--- ledger_get-stock-prices.sh
+++ ledger_get-stock-prices.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 ledger_dir=~/Accounts
-rates_file=$LEDGER_DIR/rates.journal
+all_j=$ledger_dir/all.journal
+rates_file=$ledger_dir/rates.journal
 
 # Store API in a file named alphavantage-api that states `key=ZCDCI5WLTC3WE2BX` with your API key.
 . $ledger_dir/alphavantage-api.key
@@ -8,8 +9,17 @@ rates_file=$LEDGER_DIR/rates.journal
 # I use "exchange:symbol" as the commodity name to make it easier to search online.
 # I couldn't find a way to make Alphavantage work with ISIN.
 # If you want to add more commodities, do so by adding to (NSE:|BSE:|NASDAQ:) and so on.
-hledger stats | grep -E '^Commodities' | grep -Eo '\b(NSE:|BSE:)\w*' > /tmp/hledger-stocks
-readarray -t stocks < /tmp/hledger-stocks
+if type ledger &>/dev/null
+   then
+   ledger -f $all_j commodities | grep -Eo '\b(NSE:|BSE:)\w*' > /tmp/ledger-stocks
+elif type hledger &>/dev/null
+   then
+   hledger -f $all_j stats | grep -E '^Commodities' | grep -Eo '\b(NSE:|BSE:)\w*' > /tmp/ledger-stocks
+else
+   echo "Neither ledger nor hledger is present"
+fi
+
+readarray -t stocks < /tmp/ledger-stocks
 
 for stock in "${stocks[@]}"
 do
