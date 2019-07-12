8540c6c29c0c092804c406849eb68110f6d4848b
the-solipsist <the.solipsist@gmail.com>
2019-07-12 21:36:19 +0530

    Updated to reflect changed file structure on AMFI India

--- ledger_get-indian-mf-prices.sh
+++ ledger_get-indian-mf-prices.sh
@@ -3,19 +3,19 @@ ledger_dir=$HOME/Accounts
 all_j=$ledger_dir/all.journal
 rates_file=$ledger_dir/rates.journal
 
-if type ledger 2>/dev/null
+if type ledger &>/dev/null
    then
-   ledger -f $all_j  commodities | grep -o '\bINF\w*' > /tmp/ledger-mf-commodities
-elif type hledger 2>/dev/null
+   ledger -f $all_j commodities | grep -o '\bINF\w*' > /tmp/ledger-mf-commodities
+elif type hledger &>/dev/null
    then
-   hledger stats | grep -o '\bINF\w*' > /tmp/hledger-mf-commodities
+   hledger stats | grep -o '\bINF\w*' > /tmp/ledger-mf-commodities
 else
    echo "Neither ledger nor hledger is present"
 fi
 
 curl -s https://www.amfiindia.com/spages/NAVAll.txt | \
-grep -f /tmp/hledger-mf-commodities | \
+grep -f /tmp/ledger-mf-commodities | \
 tr -d '\r' | \
-awk -F";" '{printf("\"%s\" %s ",$2,"₹"$5);system("date -d "$8" +%Y-%m-%d");}' | \
+awk -F";" '{printf("\"%s\" %s ",$2,"₹"$5);system("date -d "$6" +%Y-%m-%d");}' | \
 awk '{print "P",$3,$1,$2 }' | \
 tee -a $rates_file
