c65d8c6aa61c14c6f882e3eba6f1d67f609c8a37
the-solipsist <the.solipsist@gmail.com>
2019-07-12 21:42:21 +0530

    Updated to be able to use both ledger and hledger

--- hledger_get-indian-nps-prices.sh
+++ hledger_get-indian-nps-prices.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
-LEDGER_DIR=~/Accounts
-RATES_FILE=$LEDGER_DIR/rates.journal
+ledger_dir=$HOME/Accounts
+all_j=$ledger_dir/all.journal
+rates_file=$ledger_dir/rates.journal
 
-hledger stats | grep -o '\bSM\w*' > /tmp/hledger-nps-commodities
+if type ledger &>/dev/null
+   then
+   ledger -f $all_j commodities | grep -o '\bSM\w*' > /tmp/ledger-nps-commodities
+elif type hledger &>/dev/null
+   then
+   hledger -f $all_j stats | grep -o '\bSM\w*' > /tmp/ledger-nps-commodities
+else
+   echo "Neither ledger nor hledger is present"
+fi
 
 d=$(date -d yesterday +%d%m%Y) ;
 
@@ -10,8 +19,8 @@ curl -sL "https://npscra.nsdl.co.in/download/NAV_File_$d.zip" | \
 bsdtar -f- -xC /tmp/ ;
 
 cat /tmp/NAV_File_$d.out | \
-grep -f /tmp/hledger-nps-commodities | \
+grep -f /tmp/ledger-nps-commodities | \
 tr -d '\r' | \
 awk -F"," '{printf("\"%s\" %s ",$4,"₹"$6);system("date -d "$1" +%Y-%m-%d");}' | \
 awk '{print "P",$3,$1,$2}' | \
-tee -a $RATES_FILE
+tee -a $rates_file
