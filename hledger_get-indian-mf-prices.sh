b7e4cd6459b622940b0b14acdd825742f931c164
the-solipsist <the.solipsist@gmail.com>
2019-07-12 05:34:00 +0530

    Changed to $RATES_FILE

--- hledger_get-indian-mf-prices.sh
+++ hledger_get-indian-mf-prices.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+LEDGER_DIR=~/Accounts
+RATES_FILE=$LEDGER_DIR/rates.journal
 
-hledger stats | grep -o '\bINF\w*' > /tmp/hledger-commodities
+hledger stats | grep -o '\bINF\w*' > /tmp/hledger-mf-commodities
 
 curl -s https://www.amfiindia.com/spages/NAVAll.txt | \
-grep -f /tmp/hledger-commodities | \
+grep -f /tmp/hledger-mf-commodities | \
 tr -d '\r' | \
 awk -F";" '{printf("\"%s\" %s ",$2,"₹"$5);system("date -d "$8" +%Y-%m-%d");}' | \
 awk '{print "P",$3,$1,$2 }' | \
-tee -a rates.journal
+tee -a $RATES_FILE
