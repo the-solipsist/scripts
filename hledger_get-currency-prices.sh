90831f3e20caed379dfbab627e867856ea2503b5
the-solipsist <the.solipsist@gmail.com>
2019-07-12 15:57:35 +0530

    Auto-detect currencies + Add 3 optional services

--- hledger_get-currency-prices.sh
+++ hledger_get-currency-prices.sh
@@ -1,14 +1,40 @@
 #!/usr/bin/env bash
-#replace "#APIKEY" with the API key, which should look something like "ZCDCI5WLTC3WE2BX"
-ALPHAVANTAGE_APIKEY=#APIKEY
-BASE_CURRENCY="INR"
-FOREIGN_CURRENCIES=("AED" "AZN" "BRL" "CHF" "CLP" "EGP" "EUR" "GBP" "GTQ" "HKD" "IDR" "KES" "LKR" "MAD" "MYR" "PHP" "QAR" "SGD" "THB" "TRY" "USD" "VND" "ZAR")
-for FOREIGN_CURRENCY in "${FOREIGN_CURRENCIES[@]}"
+
+service="alphavantage"
+base_currency="INR"
+
+ledger_dir=~/Accounts
+rates_file=$ledger_dir/rates.journal
+foreign_file=$ledger_dir/foreignholdings.journal
+all_file=$ledger_dir/all.journal
+. $ledger_dir/$service-api.key
+
+curl -s https://www.alphavantage.co/physical_currency_list/ | awk -F',' '{print $1}' > /tmp/currency-list
+hledger -f $all_file -f $foreign_file stats | grep -E '^Commodities' | grep -o -f /tmp/currency-list > /tmp/valid-currencies
+readarray -t foreign_currencies < /tmp/valid-currencies
+
+n=0
+
+if [ $service = "alphavantage" ]
+    then
+    site="https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${foreign_currencies[n]}&to_currency=$base_currency&a$
+    extract_rate () { jq -r '.[]["5. Exchange Rate"]'; }
+elif [ $service = "FIXER" ]
+    then
+    site="http://data.fixer.io/api/convert?access_key=$key&from=$base_currency&to=${foreign_currencies[n]}&amount=1"
+    extract_rate () { jq -r '.result'; }
+elif [ $service = "currconv" ]
+    then
+    site="https://free.currconv.com/api/v7/convert?q=$base_currency_${foreign_currencies[n]}&compact=yes&apiKey=$key"
+    extract_rate () { jq -r '.[].val'; }
+fi
+
+for foreign_currency in "${foreign_currencies[@]}"
 do
-#  curl -s "https://api.fixer.io/latest?base=$i&symbols=$BASE_CURRENCY" | jq .rates.INR | \
-  curl -s "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=$FOREIGN_CURRENCY&to_currency=$BASE_CURRENCY&apikey=$ALPHAVANTAGE_APIKEY" | \
-  jq -r '."Realtime Currency Exchange Rate"."5. Exchange Rate"' | \
-  awk -v c=$FOREIGN_CURRENCY '{print "P",strftime("%Y-%m-%d"),c,"₹"$1}' | \
-  tee -a ~/Accounts/rates.journal
+  curl -s  $site | \
+  extract_rate | \
+  awk -v c=$foreign_currency '{print "P",strftime("%Y-%m-%d"),c,"₹"$1}' | \
+  tee -a $rates_file
+  let "n=n+1"
   sleep 10s #rate limit to keep the API happy
 done
