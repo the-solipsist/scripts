44eee94de92a1e5a21735d0f9f6f5f222763bc63
Pranesh Prakash <the.solipsist@gmail.com>
2021-10-28 21:18:04 +0530

    Added currencyscoop, and set it as default

--- ledger_get-currency-prices.sh
+++ ledger_get-currency-prices.sh
@@ -1,42 +1,62 @@
 #!/usr/bin/env bash
 
-service="alphavantage"
+service="currencyscoop"
+#service="alphavantage"
+#service="currconv"
 base_currency="INR"
 
 ledger_dir=~/accounts
-rates_file=$ledger_dir/rates.journal
+rates_file=$ledger_dir/rates_currency.journal
 foreign_file=$ledger_dir/foreignholdings.journal
 all_file=$ledger_dir/all.journal
 . $ledger_dir/scripts/$service-api.key
 
-curl -s https://www.alphavantage.co/physical_currency_list/ | awk -F',' '{print $1}' > /tmp/currency-list
-curl -s https://www.alphavantage.co/digital_currency_list/ | awk -F',' '{print $1}' >> /tmp/currency-list
-hledger -f $all_file -f $foreign_file stats | grep -E '^Commodities' | grep -w -o -f /tmp/currency-list > /tmp/valid-currencies
-readarray -t foreign_currencies < /tmp/valid-currencies
+if [ $service = "alphavantage" ]
+ then
+ curl -s https://www.alphavantage.co/physical_currency_list/ | awk -F',' '{print $1}' > /tmp/currency-list
+elif [ $service = "currencyscoop" ]
+ then
+ curl -s "https://api.currencyscoop.com/v1/currencies?api_key=${key}" | jq -r '.response.fiats[].currency_code' > /tmp/currency-list
+fi
 
-n=0
+hledger -f $all_file commodities > /tmp/ledger-commodities
+readarray -t ledger_commodities < /tmp/ledger-commodities
+rm /tmp/ledger-currencies 2> /dev/null
+
+for c in "${ledger_commodities[@]}"
+ do
+  if [[ "${#c}" == 6 || "${#c}" == 3 ]];
+  then echo ${c} | cut -b1-3 | sponge -a /tmp/ledger-currencies;
+  fi
+done
+
+grep -w -o -f /tmp/currency-list /tmp/ledger-currencies > /tmp/valid-currencies
+readarray -t foreign_currencies < /tmp/valid-currencies
 
 for foreign_currency in "${foreign_currencies[@]}"
 do
-  printf $n
   if [ $service = "alphavantage" ]
     then
-    sleep 15s #rate limit to keep the API happy
-    site="https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${foreign_currencies[n]}&to_currency=$base_currency&apikey=$key"
+    site="https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${foreign_currency}&to_currency=$base_currency&apikey=$key"
     extract_rate () { jq -r '.[]["5. Exchange Rate"]'; }
-  elif [ $service = "FIXER" ]
+    sleep 15s #rate limit to keep the API happy
+  elif [ $service = "fixer" ]
     then
-    site="http://data.fixer.io/api/convert?access_key=$key&from=$base_currency&to=${foreign_currencies[n]}&amount=1"
+    site="http://data.fixer.io/api/convert?access_key=$key&from=$base_currency&to=${foreign_currency}&amount=1"
     extract_rate () { jq -r '.result'; }
   elif [ $service = "currconv" ]
     then
-    site="https://free.currconv.com/api/v7/convert?q=$base_currency_${foreign_currencies[n]}&compact=yes&apiKey=$key"
-    extract_rate () { jq -r '.[].val'; }
+    site="https://free.currconv.com/api/v7/convert?q=${foreign_currency}_${base_currency}&compact=yes&apiKey=$key"
+    extract_rate () { jq -r '.results[].val'; }
+  elif [ $service = "currencyscoop" ]
+    then
+    site="https://api.currencyscoop.com/v1/convert?from=${foreign_currency}&to=${base_currency}&amount=1&api_key=${key}"
+    extract_rate () { jq -r '.response.value'; }
   fi
-  printf $site
+  printf "$site\n"
   curl -s $site | \
   extract_rate | \
   awk -v c=$foreign_currency '{print "P",strftime("%Y-%m-%d"),c,"₹"$1}' | \
-  tee -a $rates_file
-  let "n=n+1"
+  sponge -a $rates_file
 done
+awk ' !x[$0]++' $rates_file | tail -n 10
