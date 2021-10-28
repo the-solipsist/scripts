523ae4096878fc08d8f4fa970058640657f8a6f6
Pranesh Prakash <the.solipsist@gmail.com>
2021-10-28 21:15:05 +0530

    Use hledger stats instead of parsing the journal

--- ledger_get-indian-nps-prices.sh
+++ ledger_get-indian-nps-prices.sh
@@ -4,19 +4,21 @@
 # The scheme IDs follow the pattern SMXXXYYY 
 #     where XXX = PFM number (from 001 to 010, as of 2021-01-28) 
 #     and YYY = scheme number (from 001 to 014, depending on which schemes are offered by the PFM)
-ledger_dir=$HOME/accounts
-all_file=$ledger_dir/all.journal
-rates_file=$ledger_dir/rates_nps.journal
+LEDGER_DIR=~/accounts
+RATES_FILE=$LEDGER_DIR/rates_nps.journal
+#all_file=$ledger_dir/all.journal
 
-if type ledger &>/dev/null
-   then
-   ledger -f $all_file commodities | grep -o '\bSM\w*' > /tmp/ledger-nps-commodities
-elif type hledger &>/dev/null
-   then
-   hledger -f $all_file commodities | grep -o '\bSM\w*' > /tmp/ledger-nps-commodities
-else
-   printf "Neither ledger nor hledger is present"
-fi
+#if type ledger &>/dev/null
+#   then
+#   ledger -f $all_file commodities | grep -o '\bSM\w*' > /tmp/ledger-nps-commodities
+#elif type hledger &>/dev/null
+#   then
+#   hledger -f $all_file commodities | grep -o '\bSM\w*' > /tmp/ledger-nps-commodities
+#else
+#   printf "Neither ledger nor hledger is present"
+#fi
+
+hledger stats | grep "^Commodities" | grep -o '\bSM\w*' > /tmp/hledger-nps-commodities
 
 n=0
 
@@ -25,8 +27,7 @@ do
   d=$(date -d "- $n days" +%d%m%Y)
   CODE=$(curl -sL -w '%{http_code}' "https://npscra.nsdl.co.in/download/NAV_File_$d.zip" -o "/tmp/NAV_File_$d.zip")
   if [[ $CODE = 404 ]]; then
-    n=$[$n+1];
-    rm "/tmp/NAV_File_$d.zip";
+    n=$[$n+1]
   else
     n=-1
   fi
@@ -34,10 +35,12 @@ done
 
 bsdtar -xf "/tmp/NAV_File_$d.zip" -C /tmp/ ;
 
-cat /tmp/NAV_File*.out | \
-grep -f /tmp/ledger-nps-commodities | \
+cat /tmp/NAV*.out | \
+grep -f /tmp/hledger-nps-commodities | \
+sort | \
 tr -d '\r' | \
-awk -F"," '{printf("\"%s\" %s ",$4,"₹"$6);system("date -d "$1" +%Y-%m-%d");}' | \
-awk '{print "P",$3,$1,$2}' | \
-tee -a $rates_file
+awk -F"," '{printf("\"%s\";%s;%s;",$4,"₹"$6,$5);system("date -d "$1" +%Y-%m-%d");}' | \
+awk -F';' '{print "P",$4,$1,$2 "\t""  ; "$3}' | \
+sponge -a $RATES_FILE
 rm /tmp/NAV*.out
+awk ' !x[$0]++' $RATES_FILE | tail -n 6
