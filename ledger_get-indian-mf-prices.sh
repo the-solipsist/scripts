6d4221b3d346feabd0a7b402d5190e72aff5f52d
Pranesh Prakash <the.solipsist@gmail.com>
2021-10-28 21:10:06 +0530

    Improve detection and add dedup

--- ledger_get-indian-mf-prices.sh
+++ ledger_get-indian-mf-prices.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-ledger_dir=$HOME/Accounts
+ledger_dir=$HOME/accounts
 all_file=$ledger_dir/all.journal
-rates_file=$ledger_dir/rates.journal
+rates_file=$ledger_dir/rates_mf.journal
 
 if type ledger &>/dev/null
    then
    ledger -f $all_file commodities | grep -o '\bINF\w*' > /tmp/ledger-mf-commodities
 elif type hledger &>/dev/null
    then
-   hledger -f $all_file stats | grep -o '\bINF\w*' > /tmp/ledger-mf-commodities
+   hledger -f $all_file stats | grep "^Commodities" | grep -o '\bINF\w*' > /tmp/ledger-mf-commodities
 else
    echo "Neither ledger nor hledger is present"
 fi
@@ -16,6 +16,7 @@ fi
 curl -s https://www.amfiindia.com/spages/NAVAll.txt | \
 grep -f /tmp/ledger-mf-commodities | \
 tr -d '\r' | \
-awk -F";" '{printf("\"%s\" %s ",$2,"₹"$5);system("date -d "$6" +%Y-%m-%d");}' | \
-awk '{print "P",$3,$1,$2 }' | \
-tee -a $rates_file
+awk -F";" '{printf("\"%s\";%s;%s;",$2,"₹"$5,$4);system("date -d "$6" +%Y-%m-%d")}' | \
+awk -F";" '{print "P",$4,$1,$2 "\t""  ; "$3}' | \
+sponge -a $rates_file
+awk ' !x[$0]++' $rates_file | tail -n 20
