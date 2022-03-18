d7782b4d89f3cbea481bb44abd9c3c55841e5c67
Pranesh Prakash <the.solipsist@gmail.com>
2022-03-19 03:38:34 +0530

    Full rewrite of the script
    * only download bhavcopy if it isn't already present
    * only create output file if status if remote file is present
    * uses gunzip instead of bsdtar.
    * print_rates is a separate function

--- ledger_get-indian-stock-prices.sh
+++ ledger_get-indian-stock-prices.sh
@@ -1,42 +1,50 @@
 #!/usr/bin/env bash
 
-LEDGER_DIR=~/accounts
-RATES_FILE=$LEDGER_DIR/rates_stocks.journal
-
-hledger commodities | grep -o '\bIN\w*' > /tmp/hledger-stock-commodities
-
+ledger_dir=~/accounts
+rates_file=$ledger_dir/rates_stocks.journal
+url="https://www1.nseindia.com/content/historical/EQUITIES"
+ua="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0"
+ref="https://www1.nseindia.com/products/content/equities/equities/archieve_eq.htm"
+date=$(date "+%d%^^b%Y")
 n=0
-URL="https://www1.nseindia.com/content/historical/EQUITIES"
-UA="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
-REF="https://www1.nseindia.com/products/content/equities/equities/archieve_eq.htm"
 
-while [ $n -gt -1 ]
-do
-  date=$(date -d "- $n days" +%d)
-  month=$(date -d "- $n days" +%b)
-  month=${month^^}
-  year=$(date -d "- $n days" +%Y)
-  CODE=$(curl -sL -w '%{http_code}' "$URL/$year/$month/cm$date$month${year}bhav.csv.zip" --referer "$REF" --user-agent "$UA" --output "/tmp/cm${date}${month}${year}bhav.csv.zip")
-   if [[ $CODE = 200 ]]; then
-    n=-1
-    echo $CODE
-   else
-    n=$[$n+1]
-    echo $REF
-    echo "$URL/$year/$month/cm${date}${month}${year}bhav.csv.zip"
-    echo $CODE
-   fi
-done
+print_rates() {
+  gunzip </tmp/cm${date}bhav.csv.zip \
+    | grep -f /tmp/hledger-stock-commodities \
+    | sort \
+    | tr -d '\r' \
+    | awk -F"," '{printf("\"%s\";%s;%s;",$13,"₹"$6,$1);system("date -d "$11" -I");}' \
+    | awk -F";" '{print "P",$4,$1,$2 "\t""  ; "$3}' \
+    | sponge -a $rates_file
+}
 
-bsdtar -xf "/tmp/cm$date$month${year}bhav.csv.zip" -C /tmp/ ;
+hledger commodities | grep -E '^IN.[0-9]+' > /tmp/hledger-stock-commodities
 
-cat /tmp/cm*.csv | \
-grep -f /tmp/hledger-stock-commodities | \
-sort | \
-tr -d '\r' | \
-awk -F"," '{printf("\"%s\";%s;%s;",$13,"₹"$6,$1);system("date -d "$11" +%Y-%m-%d");}' | \
-awk -F";" '{print "P",$4,$1,$2 "\t""  ; "$3}' | \
-sponge -a $RATES_FILE
-rm /tmp/cm*.csv
-awk ' !x[$0]++' $RATES_FILE | sponge $RATES_FILE
-tail -n 10 $RATES_FILE
+if [ -s /tmp/cm${date}bhav.csv.zip ]; then # don't download if already downloaded
+  tail -n 10 $rates_file
+  exit
+else
+  while [ $n -gt -1 ];  do
+    date=$(date -d "- $n days" "+%d%^^b%Y")
+    month=$(date -d "-$n days" "+%^^b")
+    year=$(date -d "- $n days" "+%Y")
+    full_url="$url/$year/$month/cm${date}bhav.csv.zip"
+    curl_opts=( --silent --referer "$ref" --user-agent "$ua" \
+      --location --output-dir "/tmp" --fail --remote-name)
+    code=$(curl -w "%{http_code}" "${curl_opts[@]}" "$full_url")
+    if [[ $code = 200 ]]; then
+      n=-1
+      echo $full_url
+      echo $code
+    else
+      ((n=n+1))
+      echo $full_url
+      echo $code
+    fi
+  done
+fi
+
+print_rates
+awk ' !x[$0]++' $rates_file \
+  | sponge $rates_file
+tail -n 10 $rates_file
