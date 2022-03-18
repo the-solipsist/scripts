d270f13029562a64b59abe5aa8e9bdecc1f0a5f3
Pranesh Prakash <the.solipsist@gmail.com>
2022-03-19 03:41:37 +0530

    Don't run script if already up-to-date

--- ledger_get-indian-mf-prices.sh
+++ ledger_get-indian-mf-prices.sh
@@ -1,22 +1,39 @@
 #!/usr/bin/env bash
 ledger_dir=$HOME/accounts
-all_file=$ledger_dir/all.journal
+latest_file=$ledger_dir/latest.journal
 rates_file=$ledger_dir/rates_mf.journal
+date=$(date -I)
 
-if type ledger &>/dev/null
-   then
-   ledger -f $all_file commodities | grep -o '\bINF\w*' > /tmp/ledger-mf-commodities
-elif type hledger &>/dev/null
-   then
-   hledger -f $all_file stats | grep "^Commodities" | grep -o '\bINF\w*' > /tmp/ledger-mf-commodities
+print_rates() {
+  grep -f /tmp/ledger-mf-commodities /tmp/NAVAll_${date}.txt \
+    | tr -d '\r' \
+    | awk -F";" '{printf("\"%s\";%s;%s;",$2,"₹"$5,$4);system("date -d "$6" +%Y-%m-%d")}' \
+    | awk -F";" '{print "P",$4,$1,$2 "\t""  ; "$3}' \
+    | sponge -a $rates_file
+}
+
+deduplicate_rates() {
+  awk ' !x[$0]++' $rates_file \
+    | sponge $rates_file
+}
+
+if type hledger &>/dev/null; then
+#   hledger -f $lates_file stats | grep "^Commodities" | grep -o '\bINF\w*' > /tmp/ledger-mf-commodities
+#    hledger bal assets --layout=bare -0 -N | awk '{print $2}' | grep -o '\bINF\w*' > /tmp/ledger-mf-commodities
+  hledger -f $latest_file commodities | grep '^INF' > /tmp/ledger-mf-commodities
+elif type ledger &>/dev/null; then
+   ledger -f $latest_file commodities | grep -o '\bINF\w*' > /tmp/ledger-mf-commodities
 else
    echo "Neither ledger nor hledger is present"
 fi
 
-curl -s https://www.amfiindia.com/spages/NAVAll.txt | \
-grep -f /tmp/ledger-mf-commodities | \
-tr -d '\r' | \
-awk -F";" '{printf("\"%s\";%s;%s;",$2,"₹"$5,$4);system("date -d "$6" +%Y-%m-%d")}' | \
-awk -F";" '{print "P",$4,$1,$2 "\t""  ; "$3}' | \
-sponge -a $rates_file
-awk ' !x[$0]++' $rates_file | tail -n 20
+if [ -s /tmp/NAVAll_${date}.txt ]; then
+  tail -n 26 $rates_file
+  exit
+else
+  curl https://www.amfiindia.com/spages/NAVAll.txt -o /tmp/NAVAll_${date}.txt
+fi
+
+print_rates
+deduplicate_rates
+tail -n 26 $rates_file
