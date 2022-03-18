06464453b23dba27cacf521ba4f35ba41bb348f5
Pranesh Prakash <the.solipsist@gmail.com>
2022-03-19 03:58:13 +0530

    Rewrite script
    * Only create output file if remote file exists
    * Use gunzip instead of bsdtar

--- ledger_get-indian-nps-prices.sh
+++ ledger_get-indian-nps-prices.sh
@@ -1,46 +1,48 @@
 #!/usr/bin/env bash
-#===================================================================================================
+###############################################################################
+# Script to download latest rates for NPS schemes and save the price in ledger
+#  format
 # Use the scheme ID (e.g. "SM001001") as the commodity symbol for the NPS units.
-# You can find the scheme ID from this website: https://npscra.nsdl.co.in/nav-search.php
-# The scheme IDs follow the pattern SMXXXYYY 
-#     where XXX = PFM number (from 001 to 010, as of 2021-01-28) 
-#     and YYY = scheme number (from 001 to 014, depending on which schemes are offered by the PFM)
-#===================================================================================================
+# You can find the scheme ID from this website:
+#  https://npscra.nsdl.co.in/nav-search.php
+# The scheme IDs follow the pattern SMXXXYYY
+#  where XXX = PFM number (from 001 to 010, as of 2021-01-28)
+#  and YYY = scheme number (from 001 to 014, depending on which
+#  schemes are offered by the PFM)
+###############################################################################
 ledger_dir=~/accounts
 rates_file=$ledger_dir/rates_nps.journal
-#journal=$ledger_dir/latest.journal
-
-#if type ledger &>/dev/null
-#   then
-#   ledger -f $journal commodities | grep -o '\bSM\w*' > /tmp/ledger-nps-commodities
-#elif type hledger &>/dev/null
-#   then
-#   hledger -f $journal commodities | grep -o '\bSM\w*' > /tmp/ledger-nps-commodities
-#else
-#   printf "Neither ledger nor hledger is present"
-#fi
 
 hledger commodities | grep -o '\bSM\w*' > /tmp/hledger-nps-commodities
 
 n=0
-while [[ $n -gt -1 ]]; do
-  d=$(date -d "- $n days" +%d%m%Y)
-  url="https://npscra.nsdl.co.in/download/NAV_File_$d.zip"
-  code=$(curl -sL -w '%{http_code}' "$url.zip" -O --output-dir /tmp/)
-  if [[ $code = 404 ]]; then
-    (( n=n+1 ))
-  else
-    n=-1
-  fi
-done
+date=$(date "+%d%m%Y")
 
-bsdtar -xf "/tmp/NAV_File_$d.zip" -C /tmp/ ;
+print_rate() {
+  gunzip -k </tmp/NAV_File_$d.zip \
+    | grep -f /tmp/hledger-nps-commodities \
+    | sort \
+    | awk -F"," '{printf("\"%s\";%s;%s;",$4,"₹"$6,$5);system("date -d "$1" --iso-8601");}' \
+    | awk -F';' '{print "P",$4,$1,$2 "\t""  ; "$3}' \
+    | sponge -a $rates_file
+}
 
-grep -f /tmp/hledger-nps-commodities /tmp/NAV*.out \
-  | sort \
-  | awk -F"," '{printf("\"%s\";%s;%s;",$4,"₹"$6,$5);system("date -d "$1" --iso-8601");}' \
-  | awk -F';' '{print "P",$4,$1,$2 "\t""  ; "$3}' \  
-  | sponge -a $rates_file
-rm /tmp/NAV*.out
+if [ -s /tmp/NAV_File_${date}.zip ]; then
+  tail -n 6 $rates_file
+  exit
+else
+  while [[ $n -gt -1 ]]; do
+    d=$(date -d "- $n days" "+%d%m%Y")
+    url="https://npscra.nsdl.co.in/download/NAV_File_$d.zip"
+    code=$(curl -fOL -w '%{http_code}' "$url" --output-dir /tmp/)
+    if [[ $code = 404 ]]; then
+      (( n=n+1 ))
+    else
+      n=-1
+      print_rate
+    fi
+  done
+fi
+
 awk ' !x[$0]++' $rates_file | sponge $rates_file
 tail -n 6 $rates_file
